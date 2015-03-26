02ce6ba118f63db494b828f33265c9fd310766d2
Benjamin Kollek <benkollek@gmail.com>
2015-03-26 13:44:37 +0000

    Testing: board has two players when created

--- spec/feature/game_setup_feature_spec.rb
+++ spec/feature/game_setup_feature_spec.rb
@@ -1,5 +1,18 @@
 require 'capybara/rspec'
-require 'game.rb'
+require 'game'
+require 'player'
+require 'board'
+require 'cell'
 
-feature 'The game can be setted up' do
+feature 'The game can be set up' do
+  let(:board1) { Board.new 10, Cell }
+  let(:board2) { Board.new 10, Cell }
+  let(:game) { Game.new player_1 }
+  let(:player_1) { Player.new board1 }
+  let(:player_2) { Player.new board2 }
+
+  it 'has two players when created' do
+    expect(game.player_1).to eq player_1
+    expect(game.player_2).to eq player_2
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,8 +3,10 @@ require 'game'
 describe Game do
   let(:game) { Game.new player_1 }
   let(:player_1) { double :player_1 }
+  let(:player_2) { double :player_2 }
 
-  it 'has a player' do
+  it 'has two players when created' do
     expect(game.player_1).to eq player_1
+    expect(game.player_1).to eq player_2
   end
 end
